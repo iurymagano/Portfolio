4084ff486ab689f931fc59d3784c9cf87a270eac
Iury <iurymagano17@live.com>
2022-03-20 01:55:47 -0300

    section project concluido

--- js/script.js
+++ js/script.js
@@ -2,19 +2,23 @@ const el = document.querySelector('#welcome');
 const text = "Seja bem-vindo ao meu portfólio!"
 const interval = 200;
 
+const img = document.querySelector('.img-projeto');
+
 const showText = (el, text, interval) => {
-
+    
     const char = text.split("").reverse();
-
+    
     const typer = setInterval(() => {
         if(!char.length) {
             return clearInterval(typer);
         }
         const next = char.pop();
-    
+        
         el.innerHTML += next;
     }, interval)
     
 }
 
+
 showText(el, text, interval);
+
